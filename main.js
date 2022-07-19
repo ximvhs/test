c4fbf2fe6afdb27fe212e1c42b77683434501188
Vu Hong Son <ximvhs26092002@gmail.com>
2022-07-19 18:54:08 +0700

    upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function sum(a, b) {
+    return a*1 + b;
+}
+
+console.log(sum(2, 5))
+
+function sum(a, b) {
+    return a + b;
+}
+
+console.log(sum(2, 3))
+
+function triple(x) {
+    return x*3;
+}
+
+var nickName = "Ximvhs";
+
+console.log(nickName.charAt(0))
+
